acdb06a3e9994917aacccb30b28f97388cf92ade
Arfan Abid <iffionex111@gmail.com>
2025-01-11 20:22:16 +0500

    Dockerizing the app

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,7 +3,10 @@ import {DB_NAME} from "../constants.js";
 
 const connectDB=async ()=>{
     try {
-        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        // const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance=await mongoose.connect(process.env.MONGODB_URI,{
+            dbName:DB_NAME
+        });
         console.log(`MongoDB Connected !! DB HOST : ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MongoDB Connection FAILED",error);
